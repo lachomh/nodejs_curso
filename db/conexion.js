c224018ae8a1baef794a2912bcd96bfdfd3c85f0
Horacio Maldonado Hernandez <lachomh@gmail.com>
2021-08-19 18:15:21 -0500

    Conexíon con bd-mariadb

---
+++ db/conexion.js
@@ -0,0 +1,13 @@
+const mariadb = requiere("mariadb");
+
+const config = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: process.env.CONN_LIMIT,
+};
+
+const pool = mariadb.createPool(config);
+
+module.exports = pool;
