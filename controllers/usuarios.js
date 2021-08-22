fe9b357ae9f124da05af7b079a3eed50d1c2fab7
Horacio Maldonado Hernandez <lachomh@gmail.com>
2021-08-21 22:38:54 -0500

    Se agrego el EndPoint para inicio de sesión

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,14 +4,28 @@ const usuariosQueries = require("../models/usuarios");
 const bcryptjs = require("bcryptjs");
 
 const usuariosGet = async (req = request, res = response) => {
+  let { limite = 5, desde = 0} = req.query;  
+      
+    parseInt(desde); 
+    parseInt(limite);
+
+  if(!Number.isInteger(limite) || !Number.isInteger(desde)) {
+     res.status(400).json({msg: "No se puede realizar esta consulta"});
+     return;
+  }
+
   let conn;
 
   try {
     conn = await pool.getConnection();
 
-    const usuarios = await conn.query(usuariosQueries.selectUsuarios);
+    const usuarios = await conn.query(usuariosQueries.selectUsuarios, [
+      desde,
+      limite,
+    ]);
 
     res.json({usuarios});
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Favor de contactar al administrador.", error});
@@ -87,8 +101,39 @@ const usuariosDelete = async (req = request, res = response) => {
   } finally {
     if (conn) conn.end();
   }
+};
 
+const usuarioSignin = async (req = request, res = response) => {
+  const { email, password } = req.body;
+
+  let conn;
+
+  try{
+    conn = await pool.getConnection();
+
+    const usuarios = await conn.query(usuariosQueries.getUsuarioByEmail, [email,]);
+
+    if (usuarios.length === 0) {
+      res.status(404).json({ msg: `No se pudo encontrar el usuario ${email}.` });
+      return;
+    }
+
+    const passwordValido = bcryptjs.compareSync(password, usuarios[0].password);
+    console.log(usuarios[0].password);
+    
+    if(!passwordValido) {
+      res.status(401).json ({msg: "La contraseña no coincide" });
+    return;
+    }
+
+    res.json ({ msg: "Inicio de sesión correcto" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Favor de contactar al administrador.", error});
+  } finally {
+    if (conn) conn.end();
+  }
 };
 
 // tarea: hacer un endpoint para actualizar la contraseña
-module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete };
+module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuarioSignin,};
